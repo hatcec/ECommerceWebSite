6019296b980efde81d4fae7473881ba6695739ac
hatcec <37028859+hatcec@users.noreply.github.com>
2021-05-10 11:51:28 +0300

    Revert "Revert "Upload""
    This reverts commit 9d867b16400686b6e30d6c7ce6e38abf212eff5b.

--- ECommerceWebSite/OrderList.aspx.cs
+++ ECommerceWebSite/OrderList.aspx.cs
@@ -15,14 +15,9 @@ namespace ECommerceWebSite
         public static String ConnectString = ConfigurationManager.ConnectionStrings["MyWebsiteDB"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["AdminUser"] != null)
-            {
+           
                 BindOrderCart();
-            }
-            else
-            {
-                Response.Redirect("~/SıgnIn.aspx");
-            }
+          
             
         }
         private void BindOrderCart()
